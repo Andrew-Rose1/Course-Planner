e8ad2d3bfc0457623d60c6e3b56d91c87aa0401e
Andrew Rose <andrewrose@Andrews-MacBook-Air.local>
2020-07-27 17:32:28 -0700

    First Travis Push

--- src/main/java/CoursePlanner.java
+++ src/main/java/CoursePlanner.java
@@ -4,13 +4,57 @@ public class CoursePlanner {
   // this method prints out: whether it is possible to take all the given courses and
   // one possible schedule for the given courses
   public static void plan(int numberOfCourses, int[][] prerequisites) {
+    if (check(numberOfCourses, prerequisites)) {
+      System.out.println("It is possible to take all of the given courses.");
+    } else {
+      System.out.println("It is NOT possible to take all of the given courses.");
+    }
     return;
   }
 
+
+
   // this is a helper method for plan; it returns a boolean to indicate if a given series of courses can be possibly scheduled
   public static boolean check(int numberOfCourses, int[][] prerequisites) {
-    // remove these two lines
-    if (RightSolution.check(numberOfCourses, prerequisites)) return false;
-    else return true;
+    try {
+      int[] arr = new int[numberOfCourses];
+      HashMap<Object, Object> hashMap = new HashMap<>();
+      for (byte b = 0; b < prerequisites.length; b++) {
+        arr[prerequisites[b][0]] = arr[prerequisites[b][0]] + 1;
+        if (hashMap.containsKey(Integer.valueOf(prerequisites[b][1]))) {
+          ((List<Integer>)hashMap.get(Integer.valueOf(prerequisites[b][1]))).add(Integer.valueOf(prerequisites[b][0]));
+        } else {
+          ArrayList<Integer> arrayList = new ArrayList();
+          arrayList.add(Integer.valueOf(prerequisites[b][0]));
+          hashMap.put(Integer.valueOf(prerequisites[b][1]), arrayList);
+        }
+      }
+      LinkedList<Integer> llist = new LinkedList();
+      int i;
+      for (i = 0; i < numberOfCourses; i++) {
+        if (arr[i] == 0)
+          llist.offer(Integer.valueOf(i));
+      }
+      while (!llist.isEmpty()) {
+        i = ((Integer)llist.poll()).intValue();
+        List<Integer> list = (List)hashMap.get(Integer.valueOf(i));
+        for (byte c = 0; list != null && c < list.size(); c++) {
+          arr[((Integer)list.get(c)).intValue()] = arr[((Integer)list.get(c)).intValue()] - 1;
+          if (arr[((Integer)list.get(c)).intValue()] - 1 == 0)
+            llist.offer(list.get(c));
+        }
+      }
+      for (i = 0; i < numberOfCourses; i++) {
+        if (arr[i] != 0)
+          return false;
+      }
+      return true;
+    // Catch ArrayIndexOutOfBoundsException Error and return false
+    // Also print out an error messagie informing the user of the error
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("Error occured: ArrayIndexOutOfBoundsException");
+      return false;
+    }
   }
+
 }
--- src/main/java/PlanTest.java
+++ src/main/java/PlanTest.java
@@ -2,13 +2,47 @@ import java.util.*;
 
 public class PlanTest {
   public static void main(String[] args) {
-
+    //Testing Cases
+    CoursePlanner obj = new CoursePlanner();
+    obj.plan(4, prerequisiteGenerator(5));
+    obj.plan(12, prerequisiteGenerator(5));
+    obj.plan(5, prerequisiteGenerator(5));
   }
 
   // this method generates a list of prerequisits as a 2D array
-  private int[][] prerequisiteGenerator(int size) {
+  private static int[][] prerequisiteGenerator(int size) {
     // remove these two lines
-    int[][] result = new int[1][1];
+    int[][] result = new int[size][2];
+    for (int i = 0; i < size; i++) {
+      while (result[i][0] == result[i][1]) {
+        result[i][0] = (int)(Math.random()*10);
+        result[i][1] = (int)(Math.random()*10);
+        // Iterate through all pairs in current 2D array and check if new pair is equal to any of them
+        // If a duplicate is found, continue to change it's values until it is no longer a duplicate
+        if (i > 0) {
+          for (int l = 0; l < i; l++) {
+            while ((result[l][0] == result[i][0]) && (result[l][1] == result[i][1])) {
+              // Test print to see the duplicated value
+              //System.out.println("Found a duplicate " + result[l][0] + "," + result[l][1] + " is the same as " + result[i][0] + "," + result[i][1]);
+              result[i][0] = (int)(Math.random()*10);
+              result[i][1] = (int)(Math.random()*10);
+            }
+          }
+        }
+      }
+    }
+
+    //TESTING --- Print out the 2D array
+    for (int m = 0; m < size; m++) {
+      System.out.print("(");
+      for (int n = 0; n < 2; n++) {
+        System.out.print(result[m][n] + " ");
+      }
+      System.out.print(")");
+      System.out.println(" ");
+    }
+
+
     return result;
   }
 }
